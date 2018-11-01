4910f17d93eb6e18b6a5bafc1c6e0b73f4e99992
Finn White <37319636+finnwhite@users.noreply.github.com>
2018-10-31 21:38:28 -0700

    Add touch and mkdir commands
    Completes Server-Side JavaScript with Node.js > CP04 Working with
      Node modules > Assignment

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const terminalCommands = require( './terminal-commands.js' );
 
 terminalCommands.ls();
+
+terminalCommands.touch( 'test-touch.txt' );
+
+terminalCommands.mkdir( 'test-mkdir' ); // initial create
+//terminalCommands.mkdir( 'test-mkdir' ); // ERROR, dir exists
--- terminal-commands.js
+++ terminal-commands.js
@@ -8,3 +8,20 @@ module.exports.ls = () => {
     console.log( output );
   } );
 };
+
+module.exports.touch = ( filename ) => {
+  fs.open( filename, 'a', ( err, fd ) => {
+    if ( err ) { return console.log( err ) }
+    fs.close( fd, ( err ) => {
+      if ( err ) { return console.log( err ) }
+      console.log( `Touched ${ filename }` );
+    } );
+  } );
+};
+
+module.exports.mkdir = ( dirname ) => {
+  fs.mkdir( dirname, ( err ) => {
+    if ( err ) { return console.log( err ) }
+    console.log( `Created ${ dirname }` );
+  } );
+};
